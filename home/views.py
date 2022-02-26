695405a1b50ac56221637d2d5b7dbb4131a51828
A-yush001 <89505931+A-yush001@users.noreply.github.com>
2022-02-26 22:45:10 +0530

    Revert "Revert "added new line in views.py""
    This reverts commit 2d5ed1b970891b5aba572ef988c88d8859be0ddd.

--- home/views.py
+++ home/views.py
@@ -50,6 +50,7 @@ def signup(request):
         # myuser.is_active = False
         myuser.is_active = False
         myuser.save()
+        #added this line
         messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
         
         # Welcome Email
